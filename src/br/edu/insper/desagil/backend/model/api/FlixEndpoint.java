1c7d46d9c68d14cd2ab6b019be1c807d7454e3d7
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2021-06-22 19:18:41 -0300

    Add boilerplate

---
+++ src/br/edu/insper/desagil/backend/model/api/FlixEndpoint.java
@@ -0,0 +1,5 @@
+package br.edu.insper.desagil.backend.model.api;
+
+public class FlixEndpoint {
+
+}
---
+++ test/br/edu/insper/desagil/backend/model/MediaTest.java
@@ -0,0 +1,14 @@
+package br.edu.insper.desagil.backend.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MediaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
