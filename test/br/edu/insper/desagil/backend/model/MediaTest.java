3fa10f0d0625dce111d7c35dc8158b59dd21f449
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2021-06-23 08:38:45 -0300

    Add boilerplate

--- test/br/edu/insper/desagil/backend/model/MediaTest.java
+++ test/br/edu/insper/desagil/backend/model/MediaTest.java
@@ -1,14 +1,72 @@
 package br.edu.insper.desagil.backend.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MediaTest {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	@BeforeEach
+	void setUp() {
 	}
 
+	@Test
+	void FiveSeconds() {
+		assertEquals(false, true);
+	}
+
+	@Test
+	void FiftyFiveSeconds() {
+		assertEquals(false, true);
+	}
+
+	@Test
+	void FiveMinutesFiveSeconds() {
+		assertEquals(false, true);
+	}
+
+	@Test
+	void FiveMinutesFiftyFiveSeconds() {
+		assertEquals(false, true);
+	}
+
+	@Test
+	void FiftyFiveMinutesFiveSeconds() {
+		assertEquals(false, true);
+	}
+
+	@Test
+	void FiftyFiveMinutesFiftyFiveSeconds() {
+		assertEquals(false, true);
+	}
+
+	@Test
+	void OneHourFiveSeconds() {
+		assertEquals(false, true);
+	}
+
+	@Test
+	void OneHourFiftyFiveSeconds() {
+		assertEquals(false, true);
+	}
+
+	@Test
+	void OneHourFiveMinutesFiveSeconds() {
+		assertEquals(false, true);
+	}
+
+	@Test
+	void OneHourFiveMinutesFiftyFiveSeconds() {
+		assertEquals(false, true);
+	}
+
+	@Test
+	void OneHourFiftyFiveMinutesFiveSeconds() {
+		assertEquals(false, true);
+	}
+
+	@Test
+	void OneHourFiftyFiveMinutesFiftyFiveSeconds() {
+		assertEquals(false, true);
+	}
 }
